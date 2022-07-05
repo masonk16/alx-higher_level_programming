003faa43d54cc0c80f3cdde184610c3a732a002c
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-07-05 03:51:09 -0700

    function that removes all characters c and C from a string.

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,21 @@
 
 
 def new_in_list(my_list, idx, element):
-    copy_my_list = my_list.copy()
+    new_list = my_list[:]
     if idx < 0:
-        return copy_my_list
-    if idx >= len(copy_my_list):
-        return copy_my_list
-    copy_my_list[idx] = element
-    return 
+        return new_list
+    elif idx >= len(my_list):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
+
+
+if __name__ == '__main__':
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_element = 9
+    new_list = new_in_list(my_list, idx, new_element)
+
+    print(new_list)
+    print(my_list)
