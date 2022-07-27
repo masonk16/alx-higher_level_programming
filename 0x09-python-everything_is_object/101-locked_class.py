847ce4bf4ca722a8a7165b5391abdbd2cf5a335f
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-07-26 17:07:00 -0700

    31. int 1/3

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
+"""Defines a locked class."""
 
 
 class LockedClass:
-    """LockedClass prevents dynamic creation of attributes other than
-    `first_name`
     """
-    __slots__ = ['first_name']
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
 
-
-if __name__ == '__main__':
-    lc = LockedClass()
-    lc.first_name = "John"
-    try:
-        lc.last_name = "Snow"
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    __slots__ = ["first_name"]
