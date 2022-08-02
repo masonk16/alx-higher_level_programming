d7b563ae8089568ee083a170e790f7ae8e8556a1
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-08-02 09:20:23 -0700

    function that returns True if the object is exactly an instance of the specified class ; otherwise False.

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
+"""
+contains the MyList class
+"""
 
 
 class MyList(list):
-    def print_sorted(self):
-        """Use bubble sort to create a sorted list to print
-        Args:
-            self (list): unsorted list
-        """
-        arr = self[:]
-        done = False
-        while not done:
-            done = True
-            for i in range(len(arr)-1):
-                if arr[i] > arr[i+1]:
-                    arr[i], arr[i+1] = arr[i+1], arr[i]
-                    done = False
-        print(arr)
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
-if __name__ == '__main__':
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
