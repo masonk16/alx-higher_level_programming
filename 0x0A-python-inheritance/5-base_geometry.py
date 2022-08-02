952cbfc49ecda34993f7904ac1f7ab6665f5908a
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-08-02 09:27:24 -0700

    class BaseGeometry (based on 5-base_geometry.py).

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
+"""Defines an empty class BaseGeometry"""
 
 
 class BaseGeometry:
     """BaseGeometry empty class"""
     pass
-
-
-if __name__ == '__main__':
-    bg = BaseGeometry()
-
-    print(bg)
-    print(dir(bg))
-    print(dir(BaseGeometry))
