ffa21c56883f5f3e4ee1061f98c4822e185a8ada
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-10-03 05:04:05 -0700

    script that searches the second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-function fact (n) {
-  if (isNaN(n)) {
-    return 1;
-  }
-  if (n === 1) {
-    return 1;
-  } else {
-    return n * fact(n - 1);
-  }
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
-console.log(fact(parseInt(process.argv[2])));
+
+console.log(factorial(Number(process.argv[2])));
